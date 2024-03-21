ec2df1a31ca26c5991d7285ba7a7669c99db3677
09mymymy04 <154625329+09mymymy04@users.noreply.github.com>
2024-03-21 16:29:01 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const change = document.getElementById('btn');
+
+
+  change.addEventListener('click', ()=>{
+    document.getElementById('text').innerHTML = '<h2 id="text">ボタンがクリックされました</h2>';
+  });
