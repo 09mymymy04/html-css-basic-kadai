fe8009e2af29173d59607573de7a3ef226940972
09mymymy04 <154625329+09mymymy04@users.noreply.github.com>
2024-03-21 16:53:38 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const change = document.getElementById('btn');
+
+setTimeout(() => {
+  change.addEventListener('click', ()=>{
+    document.getElementById('text').innerHTML = 'ボタンがクリックされました';
+  });
+}, 2000);
+
